90ea8ceb01ff1a58362fac6d55640fda82803ba2
Vestyn <74372175+Vestyn@users.noreply.github.com>
2021-01-02 22:21:42 -0600

    MIgrated from https://github.com/Vestyn/rss_project

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
 # RSS Project
-This project was created during a Scripting Languages course taught at my uni. It's purpose (for now) is to print the top 5 stories from a given RSS feed.
+This project was created during a Scripting Languages course taught at my uni. It's purpose (for now) is to print the top 5 stories from a given RSS feed. I plan on expanding this project a bit more to include some additional functionality and ease-of-use. Stay tuned! -V
+
+# Prereqs
+This project uses the "feedparser" module to analyze and format feed data (.xml) for human eyes.
+
+You can install feedparser with pip by running the following command:
+pip3 install feedparser
 
 # Usage
-To use the program with a custom URL: python rss_project.py [URL]
-To use the program with pre-built URLs: python rss_project.py
+To use the program with a custom URL: 'python rss_project.py [URL]'
+To use the program with pre-built URLs: 'python rss_project.py'
+
+Current (01/2020) menu of pre-built options:
+
+1. BBC News
+2. CNN Top Stories
+3. ESPN
+4. Fox News - Latest
+5. LifeHacker
+6. New York Times
+7. NPR Top Stories
+8. Reddit r/news
+9. Reuters Financial News
+10. Wall Street Journal - World News
 
 # Disclaimer
 This project is inspired by the Mr. Robot television series. Feel free to use it as you see fit.
--- rss_project.py
+++ rss_project.py
@@ -2,73 +2,60 @@
 import feedparser           # Used for parsing though RSS feeds via url
 import sys                  # Used to obtain arguments from the cli
 
+# To Dos:
+#           Add option for custom URL in menu 
+#           Expand RSS feed library (Maybe find a library online to import feed urls from?)
+#           Create a welcome message
+
+
+
 # RSS Feed Links
-# 1. BBC Link
-url_BBC = "http://feeds.bbci.co.uk/news/rss.xml?edition=us"
-bbc_feed = feedparser.parse(url_BBC)
+bbc_feed = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml?edition=us")                      #1
 
-# 2. CNN Link
-url_CNN = "http://rss.cnn.com/rss/cnn_topstories.rss"
-cnn_feed = feedparser.parse(url_CNN)
+cnn_feed = feedparser.parse("http://rss.cnn.com/rss/cnn_topstories.rss")                            #2
 
-# 3. ESPN Link
-url_ESPN = "https://www.espn.com/espn/rss/news"
-espn_feed = feedparser.parse(url_ESPN)
+espn_feed = feedparser.parse("https://www.espn.com/espn/rss/news")                                  #3
 
-# 4. Fox News
-url_Fox = "http://feeds.foxnews.com/foxnews/latest/?feed=rss"
-fox_feed = feedparser.parse(url_Fox)
+fox_feed = feedparser.parse("http://feeds.foxnews.com/foxnews/latest/?feed=rss")                    #4
 
-# 5. LifeHacker
-url_LifeHacker = "https://lifehacker.com/rss"
-lifehacker_feed = feedparser.parse(url_LifeHacker)
+lifehacker_feed = feedparser.parse("https://lifehacker.com/rss")                                    #5
 
-# 6. New York Times
-url_NYT = "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
-nyt_feed = feedparser.parse(url_NYT)
+nyt_feed = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/World.xml")               #6
 
-# 7. NPR
-url_NPR = "https://feeds.npr.org/1001/rss.xml"
-npr_feed = feedparser.parse(url_NPR)
+npr_feed = feedparser.parse("https://feeds.npr.org/1001/rss.xml")                                   #7
 
-# 8. Reddit ( r/news )
-url_Reddit = "https://www.reddit.com/r/news/.rss"
-reddit_feed = feedparser.parse(url_Reddit)
+reddit_feed = feedparser.parse("https://www.reddit.com/r/news/.rss")                                #8
 
-# 9. Reuters
-url_Reuters = "https://ir.thomsonreuters.com/rss/news-releases.xml?items=15"
-reuters_feed = feedparser.parse(url_Reuters)
+reuters_feed = feedparser.parse("https://ir.thomsonreuters.com/rss/news-releases.xml?items=15")     #9
 
-# 10. Wall Street Journal
-url_WallStreet = "https://feeds.a.dj.com/rss/RSSWorldNews.xml"
-wallstreet_feed = feedparser.parse(url_WallStreet)
+wallstreet_feed = feedparser.parse("https://feeds.a.dj.com/rss/RSSWorldNews.xml")                   #10
 
 # Check for arguments in the cli
 # Usage: python3 rss_project.py [url_of_rss_feed]
 if len(sys.argv) > 1:
-    # Convert argument to string
-    url_argument = str(sys.argv[1])
-    # Use feedparser to parse the argument
-    argument_feed = feedparser.parse(url_argument)
+    # Convert argument to string and use feedparser to parse the argument
+    argument_feed = feedparser.parse(str(sys.argv[1]))
 
-    # Print the feed title and link
+    # Test loop
+    # Prompt user for custom feed_number variable
+    number_of_feeds = int(input("Number of feeds to view: "))
+    # Variable for while loop
+    feed_number = 0
+
+    # Print the feed title and feed URL
     print ("\nTitle: " + argument_feed['feed']['title'])
     print ("Link: " + argument_feed['feed']['link'] + "\n")
 
-    # Print the top 5 entries in the feed
-    print ("1: " + argument_feed['entries'][0]['title'])
-    print ("Link: " + argument_feed['entries'][0]['link'])
-    print ("2: " + argument_feed['entries'][1]['title'])
-    print ("Link: " + argument_feed['entries'][1]['link'])
-    print ("3: " + argument_feed['entries'][2]['title'])
-    print ("Link: " + argument_feed['entries'][2]['link'])
-    print ("4: " + argument_feed['entries'][3]['title'])
-    print ("Link: " + argument_feed['entries'][3]['link'])
-    print ("5: " + argument_feed['entries'][4]['link'])
-    print ("Link: " + argument_feed['entries'][4]['link'] + "\n")
+    # Use loop to display 'feed_number' amount of feeds   
+    while feed_number < number_of_feeds:
+        print (str(feed_number + 1) + ": " + argument_feed['entries'][feed_number]['title'])
+        print ("Link: " + argument_feed['entries'][feed_number]['link'])
+        feed_number += 1
 else:
     # Create a menu for user input
     menu_choice = input("""
+    Menu
+    0. Custom URL
     1. BBC News
     2. CNN Top Stories
     3. ESPN
@@ -80,177 +67,143 @@ else:
     9. Reuters Financial News
     10. Wall Street Journal - World News
 
-    Enter the number of the RSS feed you want to view (0 to exit): """)
+    Enter the number of the RSS feed you want to view ('exit' or CTRL+C to exit): """)
 
     # Create a loop for continuous program use
-    while menu_choice != "0":
+    while menu_choice != "exit":
     # Conditional statements to handle user input
-        if menu_choice == "1":
-            # Print the feed title and link
+        if menu_choice == "0":
+            print("Coming soon...")
+        elif menu_choice == "1":
+            # Print the feed title and feed URL
             print ("\nTitle: " + bbc_feed['feed']['title'])
             print ("Link: " + bbc_feed['feed']['link'] + "\n")
 
-            # Print the top 5 entries in the feed
-            print ("1: " + bbc_feed['entries'][0]['title'])
-            print ("Link: " + bbc_feed['entries'][0]['link'])
-            print ("2: " + bbc_feed['entries'][1]['title'])
-            print ("Link: " + bbc_feed['entries'][1]['link'])
-            print ("3: " + bbc_feed['entries'][2]['title'])
-            print ("Link: " + bbc_feed['entries'][2]['link'])
-            print ("4: " + bbc_feed['entries'][3]['title'])
-            print ("Link: " + bbc_feed['entries'][3]['link'])
-            print ("5: " + bbc_feed['entries'][4]['link'])
-            print ("Link: " + bbc_feed['entries'][4]['link'])
+            feed_number = 0
+
+            # Use loop to display 'feed_number' amount of feeds   
+            while feed_number < 5:
+                print (str(feed_number + 1) + ": " + bbc_feed['entries'][feed_number]['title'])
+                print ("Link: " + bbc_feed['entries'][feed_number]['link'])
+                feed_number += 1    
         elif menu_choice == "2":
-            # Print the feed title and link
+            # Print the feed title and feed URL
             print ("\nTitle: " + cnn_feed['feed']['title'])
             print ("Link: " + cnn_feed['feed']['link'] + "\n")
-            
-            # Print the top 5 entries in the feed
-            print ("1: " + cnn_feed['entries'][0]['title'])
-            print ("Link: " + cnn_feed['entries'][0]['link'])
-            print ("2: " + cnn_feed['entries'][1]['title'])
-            print ("Link: " + cnn_feed['entries'][1]['link'])
-            print ("3: " + cnn_feed['entries'][2]['title'])
-            print ("Link: " + cnn_feed['entries'][2]['link'])
-            print ("4: " + cnn_feed['entries'][3]['title'])
-            print ("Link: " + cnn_feed['entries'][3]['link'])
-            print ("5: " + cnn_feed['entries'][4]['link'])
-            print ("Link: " + cnn_feed['entries'][4]['link'])
+
+            feed_number = 0
+
+            # Use loop to display 'feed_number' amount of feeds   
+            while feed_number < 5:
+                print (str(feed_number + 1) + ": " + cnn_feed['entries'][feed_number]['title'])
+                print ("Link: " + cnn_feed['entries'][feed_number]['link'])
+                feed_number += 1 
         elif menu_choice == "3":
-            # Print the feed title and link
+            # Print the feed title and feed URL
             print ("\nTitle: " + espn_feed['feed']['title'])
             print ("Link: " + espn_feed['feed']['link'] + "\n")
 
-            # Print the top 5 entries in the feed
-            print ("1: " + espn_feed['entries'][0]['title'])
-            print ("Link: " + espn_feed['entries'][0]['link'])
-            print ("2: " + espn_feed['entries'][1]['title'])
-            print ("Link: " + espn_feed['entries'][1]['link'])
-            print ("3: " + espn_feed['entries'][2]['title'])
-            print ("Link: " + espn_feed['entries'][2]['link'])
-            print ("4: " + espn_feed['entries'][3]['title'])
-            print ("Link: " + espn_feed['entries'][3]['link'])
-            print ("5: " + espn_feed['entries'][4]['link'])
-            print ("Link: " + espn_feed['entries'][4]['link'])
+            feed_number = 0
+
+            # Use loop to display 'feed_number' amount of feeds   
+            while feed_number < 5:
+                print (str(feed_number + 1) + ": " + espn_feed['entries'][feed_number]['title'])
+                print ("Link: " + espn_feed['entries'][feed_number]['link'])
+                feed_number += 1 
         elif menu_choice == "4":
-            # Print the feed title and link
+            # Print the feed title and feed URL
             print ("\nTitle: " + fox_feed['feed']['title'])
             print ("Link: " + fox_feed['feed']['link'] + "\n")
 
-            # Print the top 5 entries in the feed
-            print ("1: " + fox_feed['entries'][0]['title'])
-            print ("Link: " + fox_feed['entries'][0]['link'])
-            print ("2: " + fox_feed['entries'][1]['title'])
-            print ("Link: " + fox_feed['entries'][1]['link'])
-            print ("3: " + fox_feed['entries'][2]['title'])
-            print ("Link: " + fox_feed['entries'][2]['link'])
-            print ("4: " + fox_feed['entries'][3]['title'])
-            print ("Link: " + fox_feed['entries'][3]['link'])
-            print ("5: " + fox_feed['entries'][4]['link'])
-            print ("Link: " + fox_feed['entries'][4]['link'])
+            feed_number = 0
+
+            # Use loop to display 'feed_number' amount of feeds   
+            while feed_number < 5:
+                print (str(feed_number + 1) + ": " + fox_feed['entries'][feed_number]['title'])
+                print ("Link: " + fox_feed['entries'][feed_number]['link'])
+                feed_number += 1 
         elif menu_choice == "5":
-            # Print the feed title and link
+            # Print the feed title and feed URL
             print ("\nTitle: " + lifehacker_feed['feed']['title'])
             print ("Link: " + lifehacker_feed['feed']['link'] + "\n")
 
-            # Print the top 5 entries in the feed
-            print ("1: " + lifehacker_feed['entries'][0]['title'])
-            print ("Link: " + lifehacker_feed['entries'][0]['link'])
-            print ("2: " + lifehacker_feed['entries'][1]['title'])
-            print ("Link: " + lifehacker_feed['entries'][1]['link'])
-            print ("3: " + lifehacker_feed['entries'][2]['title'])
-            print ("Link: " + lifehacker_feed['entries'][2]['link'])
-            print ("4: " + lifehacker_feed['entries'][3]['title'])
-            print ("Link: " + lifehacker_feed['entries'][3]['link'])
-            print ("5: " + lifehacker_feed['entries'][4]['link'])
-            print ("Link: " + lifehacker_feed['entries'][4]['link'])
+            feed_number = 0
+
+            # Use loop to display 'feed_number' amount of feeds   
+            while feed_number < 5:
+                print (str(feed_number + 1) + ": " + lifehacker_feed['entries'][feed_number]['title'])
+                print ("Link: " + lifehacker_feed['entries'][feed_number]['link'])
+                feed_number += 1 
         elif menu_choice == "6":
-            # Print the feed title and link
+            # Print the feed title and feed URL
             print ("\nTitle: " + nyt_feed['feed']['title'])
             print ("Link: " + nyt_feed['feed']['link'] + "\n")
 
-            # Print the top 5 entries in the feed
-            print ("1: " + nyt_feed['entries'][0]['title'])
-            print ("Link: " + nyt_feed['entries'][0]['link'])
-            print ("2: " + nyt_feed['entries'][1]['title'])
-            print ("Link: " + nyt_feed['entries'][1]['link'])
-            print ("3: " + nyt_feed['entries'][2]['title'])
-            print ("Link: " + nyt_feed['entries'][2]['link'])
-            print ("4: " + nyt_feed['entries'][3]['title'])
-            print ("Link: " + nyt_feed['entries'][3]['link'])
-            print ("5: " + nyt_feed['entries'][4]['link'])
-            print ("Link: " + nyt_feed['entries'][4]['link'])
+            feed_number = 0
+
+            # Use loop to display 'feed_number' amount of feeds   
+            while feed_number < 5:
+                print (str(feed_number + 1) + ": " + nyt_feed['entries'][feed_number]['title'])
+                print ("Link: " + nyt_feed['entries'][feed_number]['link'])
+                feed_number += 1 
         elif menu_choice == "7":
-            # Print the feed title and link
+            # Print the feed title and feed URL
             print ("\nTitle: " + npr_feed['feed']['title'])
             print ("Link: " + npr_feed['feed']['link'] + "\n")
-            
-            # Print the top 5 entries in the feed
-            print ("1: " + npr_feed['entries'][0]['title'])
-            print ("Link: " + npr_feed['entries'][0]['link'])
-            print ("2: " + npr_feed['entries'][1]['title'])
-            print ("Link: " + npr_feed['entries'][1]['link'])
-            print ("3: " + npr_feed['entries'][2]['title'])
-            print ("Link: " + npr_feed['entries'][2]['link'])
-            print ("4: " + npr_feed['entries'][3]['title'])
-            print ("Link: " + npr_feed['entries'][3]['link'])
-            print ("5: " + npr_feed['entries'][4]['link'])
-            print ("Link: " + npr_feed['entries'][4]['link'])
+
+            feed_number = 0
+
+            # Use loop to display 'feed_number' amount of feeds   
+            while feed_number < 5:
+                print (str(feed_number + 1) + ": " + npr_feed['entries'][feed_number]['title'])
+                print ("Link: " + npr_feed['entries'][feed_number]['link'])
+                feed_number += 1 
         elif menu_choice == "8":
-            # Print the feed title and link
+            # Print the feed title and feed URL
             print ("\nTitle: " + reddit_feed['feed']['title'])
             print ("Link: " + reddit_feed['feed']['link'] + "\n")
 
-            # Print the top 5 entries in the feed
-            print ("1: " + reddit_feed['entries'][0]['title'])
-            print ("Link: " + reddit_feed['entries'][0]['link'])
-            print ("2: " + reddit_feed['entries'][1]['title'])
-            print ("Link: " + reddit_feed['entries'][1]['link'])
-            print ("3: " + reddit_feed['entries'][2]['title'])
-            print ("Link: " + reddit_feed['entries'][2]['link'])
-            print ("4: " + reddit_feed['entries'][3]['title'])
-            print ("Link: " + reddit_feed['entries'][3]['link'])
-            print ("5: " + reddit_feed['entries'][4]['link'])
-            print ("Link: " + reddit_feed['entries'][4]['link'])
+            feed_number = 0
+
+            # Use loop to display 'feed_number' amount of feeds   
+            while feed_number < 5:
+                print (str(feed_number + 1) + ": " + reddit_feed['entries'][feed_number]['title'])
+                print ("Link: " + reddit_feed['entries'][feed_number]['link'])
+                feed_number += 1 
         elif menu_choice == "9":
-            # Print the feed title and link
+            # Print the feed title and feed URL
             print ("\nTitle: " + reuters_feed['feed']['title'])
             print ("Link: " + reuters_feed['feed']['link'] + "\n")
 
-            # Print the top 5 entries in the feed
-            print ("1: " + reuters_feed['entries'][0]['title'])
-            print ("Link: " + reuters_feed['entries'][0]['link'])
-            print ("2: " + reuters_feed['entries'][1]['title'])
-            print ("Link: " + reuters_feed['entries'][1]['link'])
-            print ("3: " + reuters_feed['entries'][2]['title'])
-            print ("Link: " + reuters_feed['entries'][2]['link'])
-            print ("4: " + reuters_feed['entries'][3]['title'])
-            print ("Link: " + reuters_feed['entries'][3]['link'])
-            print ("5: " + reuters_feed['entries'][4]['link'])
-            print ("Link: " + reuters_feed['entries'][4]['link'])
+            feed_number = 0
+
+            # Use loop to display 'feed_number' amount of feeds   
+            while feed_number < 5:
+                print (str(feed_number + 1) + ": " + reuters_feed['entries'][feed_number]['title'])
+                print ("Link: " + reuters_feed['entries'][feed_number]['link'])
+                feed_number += 1 
         elif menu_choice == "10":
-            # Print the feed title and link
+            # Print the feed title and feed URL
             print ("\nTitle: " + wallstreet_feed['feed']['title'])
             print ("Link: " + wallstreet_feed['feed']['link'] + "\n")
 
-            # Print the top 5 entries in the feed
-            print ("1: " + wallstreet_feed['entries'][0]['title'])
-            print ("Link: " + wallstreet_feed['entries'][0]['link'])
-            print ("2: " + wallstreet_feed['entries'][1]['title'])
-            print ("Link: " + wallstreet_feed['entries'][1]['link'])
-            print ("3: " + wallstreet_feed['entries'][2]['title'])
-            print ("Link: " + wallstreet_feed['entries'][2]['link'])
-            print ("4: " + wallstreet_feed['entries'][3]['title'])
-            print ("Link: " + wallstreet_feed['entries'][3]['link'])
-            print ("5: " + wallstreet_feed['entries'][4]['link'])
-            print ("Link: " + wallstreet_feed['entries'][4]['link'])
+            feed_number = 0
+
+            # Use loop to display 'feed_number' amount of feeds   
+            while feed_number < 5:
+                print (str(feed_number + 1) + ": " + wallstreet_feed['entries'][feed_number]['title'])
+                print ("Link: " + wallstreet_feed['entries'][feed_number]['link'])
+                feed_number += 1 
         else:
             # Error handling: Request new entry
-            print("\nERROR! Invalid input!")
+            print("\nERROR! Invalid input! Please enter a number (0-10) or type 'exit' to exit")
+            print("\nRedisplaying menu...")
+            
         
     # Display the menu to re-run the program
         menu_choice = input("""
+    Menu
+    0. Custom URL
     1. BBC News
     2. CNN Top Stories
     3. ESPN
@@ -262,4 +215,6 @@ else:
     9. Reuters Financial News
     10. Wall Street Journal - World News
 
-    Enter the number of the RSS feed you want to view (0 to exit): """)
+    Enter the number of the RSS feed you want to view ('exit' or CTRL+C to exit): """)
+
+# Thanks for stopping by! <3
